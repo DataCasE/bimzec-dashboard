a301fc21e995f40c53869d624fc9252cb96e44a0
Caspar Egas <casparegas@AMS-Caspar-MacBook-Air.local>
2022-10-06 11:27:57 +0200

    x

--- 15mincity_dash.py
+++ 15mincity_dash.py
@@ -5,7 +5,6 @@ from dash import Dash, dcc, html, Input, Output, dash_table
 import plotly
 import plotly.express as px
 import dash_bootstrap_components as dbc
-import plotly.graph_objs as go
 import geopandas as gpd
 import json
 import os 
